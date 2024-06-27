97b08259e11115ded91bdafc34f1af32f230aeb4
ASUS <5646krupaparmar@gmail.com>
2024-06-28 00:09:23 +0530

    27/6/24 27-6-24

--- lib/utils/food_name_list.dart
+++ lib/utils/food_name_list.dart
@@ -7,5 +7,30 @@ List foodName = [
   {
     'category': 'summer specials',
     'type': 'ice-cream-strawberry,ice-cream-chocolate,ice-cream-choco-browni,ice-cream-philadelphia,ice-cream frozen yogurt,kulfi,kulfi-pista,kulfi-mango,kulfi-matka,kulfi-rose,falooda-rose,falooda-mango,falooda-dry fruit,falooda-chocolate,falooda-kesar pista,shake-strawberry,shake-salted caramel,shake-peanut butter paradise,shake-chocoholics dream'
+  },
+  {
+    'category': 'ice cream',
+    'type':'ice-cream vanilla,ice-cream buttered pecan,ice-cream chocolate fav,ice-cream-choco fav browni,ice-cream-strawberry fav,ice-cream mint chocolate chip,ice-cream-cookies n cream,ice-cream-pistachio'
+  },
+  {
+    'category':'crispy',
+    'type':'crispy-vegetable balls,crispy-dry samosa,crispy-masala vada,crispy-pakora,crispy-vada pav,crispy-jackfruit chips,crispy-crispy corn,crispy-tofu'
+  },
+  {
+    'category':'paratha',
+    'type':'paratha-aloo,paratha-pyaz,paratha-methi,paratha-mooli,paratha-paneer,paratha-matar,paratha-laccha,paratha-dhakai,paratha-ajwain,paratha-mix veg,paratha-palak paneer,paratha-chess paratha,paratha-paneer bhurji,paratha-gobbi,paratha-aloo matar,paratha-pizza paratha,paratha-chilli paneer paratha,paratha-bhujiya sev paratha'
+
+  },
+  {
+    'category':'pasta',
+    'type':'pasta-bucatini,pasta-ditalini,pasta-macaroni,pasta-orecchiette,pasta-orzo,pasta-radiatore,pasta-fusilli,pasta-rigatoni'
+  },
+  {
+    'category':'coffee',
+    'type':'coffee-cappuccino,coffee-cold brew,coffee-lungo,coffee-mocha,coffee-affogato,coffee-americano,coffee-latte,coffee-flat white,coffee-macchiato,coffee-irish'
+  },
+  {
+    'category':'jain food',
+    'type':'jain-samosa,jain-vada pav,jain-dhokla,jain-kachori,jain-manchurian,jain-noddles,jain-dosa,jain-masala dosa,jain-chinese roll,jain-pizza,jain-ras,jain-gulab jamun,jain-rasgulla,jain-sira,jain-handva,jain-idli,jain-paneer sabji,jain-kaju masala,jain-naan,jain-briyani,jain-dabeli'
   }
 ];
